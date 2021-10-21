763269c26592b86f9fe1d8094c810305fd1218e6
xingyifan <xingyifan10@qq.com>
2021-10-21 15:11:04 +0800

    feat:解构版apply

--- apply/apply.js
+++ apply/apply.js
@@ -35,3 +35,24 @@ function bar(name, age) {
 }
 
 console.log(bar.apply2(obj, ['sam', 12]))
+
+
+
+/*
+** 利用解构
+*/
+Function.prototype.apply = function (ctx,arr) {
+  // 在浏览器中需要指定为 window，在 node 环境中，则为 空对象
+  ctx = ctx || typeof window === 'undefined' ? {} : window
+  ctx.func = this
+
+  let result
+  if (!arr) {
+      result = ctx.func()
+  } else {
+     result = ctx.func(...arr)
+  }
+
+  delete ctx.func
+  return result
+}
